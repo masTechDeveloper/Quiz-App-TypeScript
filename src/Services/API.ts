e67e7cff7b8690dd000b8adc8b7de8a4ca08e7da
masTech Developer <ashir342@gmail.com>
2020-08-16 19:54:49 +0500

    Update API.ts

--- src/Services/API.ts
+++ src/Services/API.ts
@@ -1,8 +1,27 @@
-export const getQuizAPI = async (totalQ: number, level: string) => {
+import { QuizType, QuestionType } from '../Types/Quiz_Types';
+
+const shuffleArray = (array: any[]) =>
+  [...array].sort(() => Math.random() - 0.5);
+
+export const getQuizAPI = async (
+  totalQ: number,
+  level: string
+): Promise<QuizType[]> => {
   const res = await fetch(
     `https://opentdb.com/api.php?amount=${totalQ}&difficulty=${level}&type=multiple`
   );
 
   let { results } = await res.json();
-  return results;
+
+  const quizReturn: QuizType[] = results.map((questionObj: QuestionType) => {
+    return {
+      question: questionObj.question,
+      answer: questionObj.correct_answer,
+      option: shuffleArray(
+        questionObj.incorrect_answers.concat(questionObj.correct_answer)
+      ),
+    };
+  });
+
+  return quizReturn;
 };
