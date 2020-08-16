e8a732814924cebe7b6fb5cc6d95bed72726aee5
masTech Developer <ashir342@gmail.com>
2020-08-16 19:54:43 +0500

    Create Quiz_Types.ts

---
+++ src/Types/Quiz_Types.ts
@@ -0,0 +1,15 @@
+export type QuestionType = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuizType = {
+  question: string;
+  answer: string;
+  option: string[];
+  correct_answer: string;
+};
