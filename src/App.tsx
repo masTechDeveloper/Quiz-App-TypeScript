194ae62a56dc708a6070d4d9bb1fc2911f2c76e1
masTech Developer <ashir342@gmail.com>
2020-08-17 00:10:50 +0500

    Update App.tsx
    Score Update

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { QuizCard } from './Components/Layout/QuizCard';
 function App() {
   let [quiz, setQuiz] = useState<QuizType[]>([]);
   let [quizCount, setQuizCount] = useState(0);
+  let [score, setScore] = useState(0);
 
   useEffect(() => {
     async function fetchQuiz() {
@@ -24,11 +25,24 @@ function App() {
   ) => {
     console.log(selectedAns);
     e.preventDefault();
+
+    const currentQuestion: QuizType = quiz[quizCount];
+
+    if (selectedAns === currentQuestion.answer) {
+      setScore(++score);
+    }
+
     if (quizCount !== quiz.length - 1) {
       setQuizCount(++quizCount);
     } else {
       alert('Finish');
       setQuizCount(0);
+
+      // Calc Percentage
+      let perc = (100 / quiz.length) * score;
+      console.log(perc + '%');
+
+      setScore(0);
     }
   };
 
@@ -36,6 +50,7 @@ function App() {
 
   return (
     <div className='App'>
+      <p>{score}</p>
       <QuizCard
         options={quiz[quizCount].option}
         question={quiz[quizCount].question}
