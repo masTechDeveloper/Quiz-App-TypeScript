7cbf419d9350cacae65edb4307df7646e011eaf8
masTech Developer <ashir342@gmail.com>
2020-08-16 23:47:19 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { getQuizAPI } from './Services/API';
 import './App.css';
 import { QuizType } from './Types/Quiz_Types';
+import { QuizCard } from './Components/Layout/QuizCard';
 
 function App() {
   let [quiz, setQuiz] = useState<QuizType[]>([]);
+  let [quizCount, setQuizCount] = useState(0);
 
   useEffect(() => {
     async function fetchQuiz() {
@@ -16,7 +18,31 @@ function App() {
     fetchQuiz();
   }, []);
 
-  return <div className='App'>Hello</div>;
+  const submitHandler = (
+    e: React.FormEvent<EventTarget>,
+    selectedAns: string
+  ) => {
+    console.log(selectedAns);
+    e.preventDefault();
+    if (quizCount !== quiz.length - 1) {
+      setQuizCount(++quizCount);
+    } else {
+      alert('Finish');
+      setQuizCount(0);
+    }
+  };
+
+  if (!quiz.length) return <h3 className='App'>Loading ...</h3>;
+
+  return (
+    <div className='App'>
+      <QuizCard
+        options={quiz[quizCount].option}
+        question={quiz[quizCount].question}
+        callback={submitHandler}
+      />
+    </div>
+  );
 }
 
 export default App;
